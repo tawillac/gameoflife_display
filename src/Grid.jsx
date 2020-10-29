32dbadc61f714858996e582ab88fd65ea0f79cba
Tamara Willacker <tamara.willacker@infoteam.de>
2020-10-29 17:37:41 +0100

    Added more buttons for api calls

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -8,20 +8,22 @@ class Grid extends Component {
         super(props);
         this.state = {
             cells: [], 
-            gameRunning: false
+            gameRunning: false,
+            rules: [], 
+            pattern: ''
         }
-        this.getNextStep = this.getNextStep.bind(this);
-        this.startGame = this.startGame.bind(this);
-        this.isGameRunning = this.isGameRunning.bind(this);
     }
     render() {
-        console.log("gameRunning=" + this.state.gameRunning);
-        console.log("cells=" + this.state.cells);
+        //console.log("gameRunning=" + this.state.gameRunning);
+        //console.log("cells=" + this.state.cells);
+        //<button className={this.state.gameRunning ? "visible" : "invisible"} onClick={() => this.getNextStep()}>NEXT</button>
+
         return (
             <div className="cellContainer">
                 <span>GameOfLife</span>
                 <button onClick={() => this.startGame()}>START</button>
-                <button className={this.state.gameRunning ? "visible" : "invisible"} onClick={() => this.getNextStep()}>NEXT</button>
+                <button onClick={() => this.getNextStep()}>NEXT</button>
+                <br></br>
                 <div className="cellColumn">
                 {this.state.cells.map((vColumn,iColumn,aColumn) => {
                     return(
@@ -35,6 +37,30 @@ class Grid extends Component {
                     )
                 })}
                 </div>
+                <button onClick={() => this.addRule('SURVIVE')}>AddRule(Survive)</button>
+                <button onClick={() => this.addRule('REPRODUCTION')}>AddRule(Reproduction)</button>
+                <button onClick={() => this.addRule('OVERPOPULATION')}>AddRule(Overpopulation)</button>
+                <button onClick={() => this.addRule('UNDERPOPULATION')}>AddRule(Underpopulation)</button>
+                <br></br>
+                <button onClick={() => this.deleteRule('SURVIVE')}>DeleteRule(Survive)</button>
+                <button onClick={() => this.deleteRule('REPRODUCTION')}>DeleteRule(Reproduction)</button>
+                <button onClick={() => this.deleteRule('OVERPOPULATION')}>DeleteRule(Overpopulation)</button>
+                <button onClick={() => this.deleteRule('UNDERPOPULATION')}>DeleteRule(Underpopulation)</button>
+                <br></br>
+                <button onClick={() => this.setPattern('BLINKER')}>Pattern -> Blinker</button>
+                <button onClick={() => this.setPattern('GLIDER')}>Pattern -> Glider</button>
+                <button onClick={() => this.setStandardRules()}>SetStandardRules</button>
+                <button onClick={() => this.clearAllRules()}>ClearAllRules</button>
+                <button onClick={() => this.getRules()}>GetRules</button>
+
+                <span>Current Rules:</span>
+                <div>{this.state.rules.map((v,i,a) => {
+                    return (<span key={i}>-{v} <br></br></span>)
+                })}</div>
+                <br></br>
+                <span>Current Starting Pattern:</span>
+                <div>{this.state.pattern}</div>
+
             </div>
         )
     }
@@ -47,6 +73,9 @@ class Grid extends Component {
             }
         )
         this.isGameRunning();
+        this.getRules();
+        this.getPattern();
+
     }
 
     startGame() {
@@ -57,7 +86,73 @@ class Grid extends Component {
             }
         )
         this.isGameRunning();
+        this.getRules();
+        this.getPattern();
+
     }
+
+    setStandardRules() {
+        console.log("setStandardRules()");
+        var rules = ['REPRODUCTION', 'SURVIVE', 'OVERPOPULATION', 'UNDERPOPULATION'];
+        Service.setRules(rules);
+        this.getRules();
+
+    }
+
+    clearAllRules() {
+        console.log("clearAllRules()");
+        var rules = [];
+        Service.setRules(rules);
+        this.getRules();
+
+    }
+
+    setPattern(pattern) {
+        console.log("setPattern()");
+        Service.setPattern(pattern);
+        this.getPattern();
+    }
+
+    addRule(rule) {
+        console.log("addRule " + rule);
+        Service.addRule(rule);
+        //this.doesRuleExist(rule);
+        this.getRules();
+    }
+
+    deleteRule(rule) {
+        console.log("deleteRule " + rule);
+        Service.deleteRule(rule);
+        //doesRuleExist(rule);
+        this.getRules();
+    }
+
+    doesRuleExist(rule) {
+        Service.doesRuleExist(rule).then(
+            response =>  {
+                var r =  true === response.data;
+                console.log("doesRuleExist: " + rule + " -> " + r);
+                return r;
+            }
+        );
+    }
+
+    getRules() {
+        Service.getRules().then(
+            response => {
+                this.setState({rules: response.data});
+            }
+        )
+    }
+
+    getPattern() {
+        Service.getPattern().then(
+            response => {
+                this.setState({pattern: response.data});
+            }
+        )
+    }
+    
 
     isGameRunning() {
         console.log("isGameRunning()");
@@ -66,6 +161,8 @@ class Grid extends Component {
                 this.setState({gameRunning: response.data});
             }
         )
+        this.getRules();
+
     }
 }
 export default Grid;
--- src/service.js
+++ src/service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = 'http://localhost:8081/rest';
 
 class Service {
-
+    
     getNextStep() {
         console.log("axios -> /nextStep");
         return axios.get(`${API_URL}/nextStep`);
@@ -18,5 +18,41 @@ class Service {
         console.log("axios -> /isGameRunning");
         return axios.get(`${API_URL}/isGameRunning`);
     }
+
+    setRules(rules) {
+        console.log("axios -> /setRules"); 
+        return axios.put(`${API_URL}/setRules`, rules);
+    }
+
+    addRule(rule) {
+        console.log("axios -> /addRule " + rule); 
+        return axios.put(`${API_URL}/addRule/${rule}`);
+    }
+
+    deleteRule(rule) {
+        console.log("axios -> /deleteRule " + rule); 
+        return axios.delete(`${API_URL}/deleteRule/${rule}`);
+    }
+
+    doesRuleExist(rule) {
+        console.log("axios -> /doesRuleExist");
+        return axios.get(`${API_URL}/doesRuleExist/${rule}`);
+    }
+
+    getRules() {
+        console.log("axios -> /getRules");
+        return axios.get(`${API_URL}/getRules`);
+    }
+
+    setPattern(pattern) {
+        console.log("axios -> /setPattern " + pattern); 
+        return axios.put(`${API_URL}/setPattern/${pattern}`);
+    }
+
+    getPattern() {
+        console.log("axios -> /getPattern");
+        return axios.get(`${API_URL}/getPattern`);
+    }
 }
+
 export default new Service();
